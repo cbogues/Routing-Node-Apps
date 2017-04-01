0fa3496e5d37abe1d91cc41feb98d7e73298aa39
cbogues <YOUR-EMAIL-ADDRESS>
2017-03-31 21:59:25 -0700

    created post routes and contact form

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 var app = require('express')(),
 	port = process.env.PORT || 8080,
-	morgan = require('morgan');
+	morgan = require('morgan'),
+	bodyParser = require('body-parser');
 
 // configure
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // set routes
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
+
 // about page
 app.get('/about', function(req, res) {
 	res.json({message:'I am the about page'});
 });
+
 // contact page
 app.get('/contact', function(req, res) {
-	res.send('I am the contact page');
+	res.sendFile(__dirname + '/contact.html');
+});
+
+app.post('/contact', function(req, res) {
+	console.log(req.body);
+	res.send('hello ' + req.body.name + '! Nice to meet you!');
 });
 
 // start the server
