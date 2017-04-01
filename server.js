5c214e8535dfc5c1770307384615a440387b3304
cbogues <YOUR-EMAIL-ADDRESS>
2017-04-01 12:26:34 -0700

    created a 404 page and route

--- server.js
+++ server.js
@@ -38,6 +38,15 @@ app.get('/@:username/:post_slug', checkName, function(req, res) {
 	res.send('You are reading ' + req.params.post_slug + ' by ' + req.params.username);
 });
 
+// 404
+app.use(function(req, res, next) {
+	res.status(404);
+	res.sendFile(__dirname + '/404.html');
+});
+
+
+
+
 function authenticate( req, res, next) {
 	//make sure the user is authenticated
 	//req.params.token  - probably
