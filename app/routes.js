aaf4c3bdd10e97fb116bb2df8c0ce5324bc4a629
cbogues <YOUR-EMAIL-ADDRESS>
2017-04-02 12:01:40 -0700

    cleaned up the routes.js file

--- app/routes.js
+++ app/routes.js
@@ -5,39 +5,48 @@ var express = require('express'),
 	// export the router
 module.exports = router;
 
-// apply the routes to the router
+// routes defined here
 
-router.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname, '../index.html'));
-});
+router.get('/', showHome);
+router.get('/about', showAbout);
+router.get('/contact', showContact);
+router.post('/contact', processContact);
+router.get('/@:username/:post_slug', showProfile);
+// 404
+router.use(show404);
+
+
+// functions for routes
+function showHome(req, res) {
+	res.sendFile(path.join(__dirname, '../index.html'));	
+}
 
 // about page
-router.get('/about', function(req, res) {
+ function showAbout(req, res) {
 	res.json({message:'I am the about page'});
-});
+}
 
 // contact page
-router.get('/contact', function(req, res) {
+ function showContact(req, res) {
 	res.sendFile(path.join(__dirname,'../contact.html'));
-});
+}
 
-router.post('/contact', function(req, res) {
+ function processContact(req, res) {
 	console.log(req.body);
 	res.send('hello ' + req.body.name + '! Nice to meet you!');
-});
+}
 
 //using params in routes
-router.get('/@:username/:post_slug', function(req, res) {
+ function showProfile(req, res) {
 	console.log(req.params);
 
 	//grab user profile
 	//grab the post based off of the post_slug
 	res.send('You are reading ' + req.params.post_slug + ' by ' + req.params.username);
-});
+}
 
-// 404
-router.use(function(req, res, next) {
+function show404(req, res, next) {
 	res.status(404);
 	res.sendFile(__dirname + '../404.html');
-});
+}
 
